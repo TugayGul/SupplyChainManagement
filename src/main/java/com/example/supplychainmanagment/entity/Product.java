8b9ebedaf53593a932a4d6f1adfc32b99d8744a3
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-15 02:42:15 +0300

    add entity classes and orm

---
+++ src/main/java/com/example/supplychainmanagment/entity/Product.java
@@ -0,0 +1,31 @@
+package com.example.supplychainmanagment.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+
+    @Column(name = "productname")
+    private String productName;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private double price;
+
+    // Getters and setters for each field
+    public int getProductId() { return productId; }
+    public void setProductId(int productId) { this.productId = productId; }
+    public String getProductName() { return productName; }
+    public void setProductName(String productName) { this.productName = productName; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+}
