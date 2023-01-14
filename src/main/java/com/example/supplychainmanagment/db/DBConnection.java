24fe903fd899c6e8472982cc316cb6585711987e
Tugay Gül <51514616+sondertg@users.noreply.github.com>
2023-01-15 01:35:33 +0300

    add db connection test

---
+++ src/main/java/com/example/supplychainmanagment/db/DBConnection.java
@@ -0,0 +1,74 @@
+package com.example.supplychainmanagment.db;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+public class DBConnection {
+    private Connection conn;
+
+    public DBConnection() {
+        try {
+            Properties prop = new Properties();
+            InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("config/db.properties");
+            prop.load(input);
+
+            String url = "jdbc:mysql://" + prop.getProperty("hostname") + ":" + prop.getProperty("port") + "/" + prop.getProperty("db_name");
+            String username = prop.getProperty("username");
+            String password = prop.getProperty("password");
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet select(String sql) {
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            return stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean insert(String sql) {
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean update(String sql) {
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean delete(String sql) {
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void close() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
